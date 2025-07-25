0332854cdde1f8a6e9e56bbd8666549125094af5
Pablo Quijivix <davidquijivixsuchi@gmail.com>
2025-07-25 09:42:19 -0600

    2 commint más modificaciones

--- Actividad_8.py
+++ Actividad_8.py
@@ -1,15 +1,31 @@
 #actividad del dia viernes 25 de julio
 #crear un numero
-productos=[]
+products=[]
 
-While True:
+while True:
     print("1.Agregar un producto a la lista")
     print("2.Modificar un producto existente")
     print("3.Eliminar un producto existente")
     print("4.Ver todos los productos")
     print("4. salir del programa")
 
-match opciones:
+    opcions= input("Eliga una opción 1-5: ")
 
-    case "1":
+    match opciones:
+        case "1":
+            nuevo= input("Digite el nueveo elemento: ")
+            products.append(nuevo)
+            print("Nuevo elemento guardado")
+
+        case "2":
+            print(f"lista actual:{products} ")
+            indice =int(input("Ingrese la posicion del elemntos que desea modificar: "))
+            new_value= input("Ingrese nuevo valor para modificar: ")
+            products[indice]=new_value
+            print("Elemento modificado con exito")
+
+        case "3":
+
+
+
 
