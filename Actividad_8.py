d0c2c7c4f476fd65da383e00d0bf3c27a5e37e86
Pablo Quijivix <davidquijivixsuchi@gmail.com>
2025-07-25 09:33:07 -0600

    1 commit opciones de menu

---
+++ Actividad_8.py
@@ -0,0 +1,15 @@
+#actividad del dia viernes 25 de julio
+#crear un numero
+productos=[]
+
+While True:
+    print("1.Agregar un producto a la lista")
+    print("2.Modificar un producto existente")
+    print("3.Eliminar un producto existente")
+    print("4.Ver todos los productos")
+    print("4. salir del programa")
+
+match opciones:
+
+    case "1":
+
